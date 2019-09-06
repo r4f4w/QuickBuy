1ba677a5c878bb758d8147a8d0a71966ad51c8e9
Rafael Wille <rafarwille@hotmail.com>
2019-09-06 18:12:12 -0300

    metade aula 19

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+    }
+}
